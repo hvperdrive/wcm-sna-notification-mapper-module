5f16faf9e0393c173361d762ec3ee6648f438738
jochemjanssens <janssens.jochem@gmail.com>
2018-03-29 16:51:43 +0200

    eslint + correct data

--- app/helpers/mappers/nieuws.js
+++ app/helpers/mappers/nieuws.js
@@ -1,16 +1,17 @@
 module.exports = (data) => {
-
 	// MAP DATA HERE!
-	if(data.medium.app === false){
+	if (data.fields.medium.app === false) {
 		return;
 	}
 
 	const title = {};
 	const description = {};
-	if(data.url.en){
-		title.en = data.url.en.description;
-		const descriptionContent = data.description.en;
+
+	if (data.fields.url.en) {
+		title.en = data.fields.url.en.description;
+		const descriptionContent = data.fields.description.en;
 		const cleanDescription = descriptionContent.replace(/<\/?[^>]+(>|$)/g, "");
+
 		description.en = cleanDescription;
 	} else {
 		// En content is verplicht
@@ -18,38 +19,41 @@ module.exports = (data) => {
 		description.en = " ";
 	}
 
-	if(data.url.nl){
-		title.nl = data.url.nl.description;
-		const descriptionContent = data.description.nl;
+	if (data.fields.url.nl) {
+		title.nl = data.fields.url.nl.description;
+		const descriptionContent = data.fields.description.nl;
 		const cleanDescription = descriptionContent.replace(/<\/?[^>]+(>|$)/g, "");
+
 		description.nl = cleanDescription;
 	}
-	if(data.url.fr){
-		title.fr = data.url.fr.description;
-		const descriptionContent = data.description.fr;
+	if (data.fields.url.fr) {
+		title.fr = data.fields.url.fr.description;
+		const descriptionContent = data.fields.description.fr;
 		const cleanDescription = descriptionContent.replace(/<\/?[^>]+(>|$)/g, "");
+
 		description.fr = cleanDescription;
 	}
-	if(data.url.de){
-		title.de = data.url.de.description;
-		const descriptionContent = data.description.de;
+	if (data.fields.url.de) {
+		title.de = data.fields.url.de.description;
+		const descriptionContent = data.fields.description.de;
 		const cleanDescription = descriptionContent.replace(/<\/?[^>]+(>|$)/g, "");
+
 		description.de = cleanDescription;
 	}
 	
-	const lifetime = (new Date(data.expireDate) - new Date(data.sendDate))/1000;
+	const lifetime = (new Date(data.fields.expireDate) - new Date(data.fields.sendDate)) / 1000;
 
 	let message = {
 		"app_id": "a58dfb59-f1c5-4444-858e-565342c05d94",
 		"included_segments": ["Active Users"],
 		"data": {
 			"icon": "train",
-			"url": data.url.nl.url,
+			"url": data.fields.url.nl.url,
 		},
 		"headings": title,
 		"contents": description,
-		"send_after": data.sendDate,
-		"ttl": lifetime
+		"send_after": data.fields.sendDate,
+		"ttl": lifetime,
 	};
 
 	return message;
